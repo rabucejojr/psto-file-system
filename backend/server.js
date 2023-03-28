c387f20a66c95f38f635dd92e9f430cb599e4347
rabucejo <rogerhapay@gmail.com>
2023-03-28 15:16:27 +0800

    configured server.js

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const dotenv=require('dotenv').config()
+const app = express()
+const port =process.env.PORT
+
+app.listen(port,()=>{
+    console.log(`running in port ${port}`)
+})
