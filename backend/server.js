ff1436e7ed577c56b1f95d7cae32cb62af469857
rabucejo <rogerhapay@gmail.com>
2023-03-28 15:36:37 +0800

    renamed routes/document.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require('express')
-const dotenv=require('dotenv').config()
+const dotenv = require('dotenv').config()
 const app = express()
-const port =process.env.PORT
+const port = process.env.PORT
+const mongoose = require('mongoose')
+const cors =require('cors')
 
-app.listen(port,()=>{
+//middleware
+app.use(express.json())
+//cors
+app.use(cors())
+//routes
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  app.listen(port, () => {
     console.log(`running in port ${port}`)
+  })
 })
