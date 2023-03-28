145f9568905ebebe9bdee4d78fb5b740e9fd2cb0
rabucejo <rogerhapay@gmail.com>
2023-03-28 16:04:40 +0800

    configured model and controller

--- backend/controllers/documentController.js
+++ backend/controllers/documentController.js
@@ -1,2 +1,13 @@
 const mongoose=require('mongoose')
+const Document =require('../models/documentModel')
 
+// get all document
+const getDocs=async(req,res)=>{
+    const docs=await Document.find({}).sort({createdAt:-1})
+    res.status(200).json(docs)
+}
+
+
+// exports
+
+module.exports={getDocs}
--- backend/models/documentModel.js
+++ backend/models/documentModel.js
@@ -1,1 +1,19 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const documentSchema = new Schema(
+  {
+    filename: {
+      type: String,
+      required: true,
+    },
+    desc: { 
+        type: String,
+        required:true, 
+    },
+    path: {
+        type: String,
+    },
+  },
+  { timestamps: true }
+)
+module.exports=mongoose.model('Document',documentSchema)
