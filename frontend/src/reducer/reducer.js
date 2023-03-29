5b004404b44965908a3b71450a12e1fa22444e52
rabucejo <rogerhapay@gmail.com>
2023-03-29 12:34:07 +0800

    add reducer.js

---
+++ frontend/src/reducer/reducer.js
@@ -0,0 +1,10 @@
+import { ACTION_TYPE } from "./actionTypes";
+
+const reducer =(state,action)=>{
+    switch(action.type){
+        case ACTION_TYPE.ADD_DOCS:
+            return{
+                
+            }
+    }
+}
